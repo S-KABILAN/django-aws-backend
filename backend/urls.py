10f7a705ec2b762a233e60906efd37b37a57defc
kabilan-s <kabilanselvakumar313@gmail.com>
2025-10-23 07:53:29 +0530

    requirements added

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Hello, world!"})
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path("", home),
 ]
